10563f524025b9e5c029f0a8d69272a46e331f57
vamshi <vamshis2017@gmail.com>
2022-11-10 23:49:00 +0530

    added code

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
 
+
 const App = () => {
   return (
     <div id="main">
-      < />
-      < />
-      < />
-      < />
+      <Heading.js/>
+      <InputQuery.js/>
+      <SubHeading.js/>
+      <SubmitButton.js/>
     </div>
   )
 }
